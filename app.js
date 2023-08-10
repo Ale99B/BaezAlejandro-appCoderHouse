e1f81e35c6406a4a3602697d73e4aa0f01a3c654
velas eunoia <baez2385@gmail.com>
2023-08-10 00:21:39 -0300

    agrengo funcion y return

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 let nombre = '';
 let telefono = '';
-nombre = prompt('Ingrese su nombre');
-while (Number(nombre) || nombre == '') {
-    if (nombre == '') {
-        nombre = prompt('No puede estar vacío, ingrese su nombre');
-    }else{
-        nombre = prompt('Su nombre no puede contener números, ingrese su nombre');
+
+
+console.log(`Su nombre es: ${pedirNombre(nombre)} y su n° de teléfono es: ${pedirTelefono(telefono)}`);
+
+function pedirNombre(nombre) {
+    nombre = prompt('Ingrese su nombre');
+    while (Number(nombre) || nombre == '') {
+        if (nombre == '') {
+            nombre = prompt('No puede estar vacío, ingrese su nombre');
+        }else{
+            nombre = prompt('Su nombre no puede contener números, ingrese su nombre');
+        } 
     }
+    return nombre;
 }
 
-
-telefono = prompt('Ingrese su n° de telefono');
-while (!Number(telefono) || telefono == '') {
-    if(!Number(telefono) && telefono != '') {
-        telefono = prompt('Debe ingresar números, ingrese su telefono');
-    }else{
-        telefono = prompt('No puede estar vacío, ingrese su telefono');
-    }
+function pedirTelefono(telefono) {
+    telefono = prompt('Ingrese su n° de telefono');
+    while (!Number(telefono) || telefono == '') {
+        if(!Number(telefono) && telefono != ''){
+            telefono = prompt('Debe ingresar números, ingrese su telefono');
+        }else{
+            telefono = prompt('No puede estar vacío, ingrese su telefono');
+        }      
+    }    
+    return telefono;
 }
-
-console.log('Su nombre es: ${nombre} y su n° de telefono es: ${telefono}');
